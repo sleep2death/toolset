d298c14d41add0afb0e904b456649afbba333794
aspirin <aspirin2d@outlook.com>
2016-07-22 00:28:50 -0400

    rename of svn tasks

--- tasks/svn-update-commit.js
+++ tasks/svn-update-commit.js
@@ -7,15 +7,15 @@ const SPRITES_PATH_OUTPUT = './data/sprites_output'
 
 const shell = require('gulp-shell')
 
-gulp.task('svn.update-sprites', shell.task([
+gulp.task('svn.update.sprites', shell.task([
   `svn update ${SPRITES_PATH}`,
   `svn update ${SPRITES_PATH_OUTPUT}`
 ], {verbose: true}))
 
-gulp.task('svn.commit-sprites', shell.task([
+gulp.task('svn.commit.sprites', shell.task([
   `svn commit ${SPRITES_PATH_OUTPUT}`
 ], {verbose: true}))
 
-gulp.task('svn.add-sprites', shell.task([
+gulp.task('svn.add.sprites', shell.task([
   `svn add --force ${SPRITES_PATH_OUTPUT}/*`
 ], {verbose: true}))
